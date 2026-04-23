f5c7bd22bcf793d826ba161a0f6ae18812e92567
Harsh BARANWAL <harsh.baranwal@amadeus.com>
2026-04-23 15:32:27 +0200

    Deleted chessBoard

--- ChessBoard/src/Q08ChessBoardInterface.java
+++
@@ -1,9 +0,0 @@
-public interface Q08ChessBoardInterface {
-    void init(Helper08 helper, String[][] chessboard);
-
-    String move(int startRow, int endRow, int startCol, int endCol);
-
-    int getGameStatus();
-
-    int getNextTurn();
-}
--- ChessBoard/src/models/moves/Move.java
+++
@@ -1,7 +0,0 @@
-//package models.moves;
-//
-//import models.Board;
-//
-//public interface Move {
-//    public boolean canMove(Board board, int startRow, int startCol, int endRow, int endCol);
-//}
